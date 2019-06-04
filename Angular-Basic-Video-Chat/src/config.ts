e955b00c05b4dbdfa2e514a004ac439b241cfc0c
AbhaySinghThakur <abhay@flytbase.com>
2019-06-04 12:43:01 +0530

    Token

--- Angular-Basic-Video-Chat/src/config.ts
+++ Angular-Basic-Video-Chat/src/config.ts
@@ -5,7 +5,7 @@ export default {
   // OR, if you have not set up a web server that runs the learning-opentok-php code,
   // set these values to OpenTok API key, a valid session ID, and a token for the session.
   // For test purposes, you can obtain these from https://tokbox.com/account.
-  API_KEY: '46210182',
-  SESSION_ID: '1_MX40NjIxMDE4Mn5-MTU1NzQxMjE0Njg0Nn5qSzBRNmw0NXNGQVJoN2RzcEpNbDJ5ZHl-UH4',
-  TOKEN: 'T1==cGFydG5lcl9pZD00NjIxMDE4MiZzaWc9MzY1MGQ0MmY4YzlhN2ViZDZhMjJmOTg1MTA5OGQ4MDRkMGU2MDAyYTpzZXNzaW9uX2lkPTFfTVg0ME5qSXhNREU0TW41LU1UVTFOelF4TWpFME5qZzBObjVxU3pCUk5tdzBOWE5HUVZKb04yUnpjRXBOYkRKNVpIbC1VSDQmY3JlYXRlX3RpbWU9MTU1OTAzMDYwOCZub25jZT0wLjMyMDkyMjUyMjM5NTE2MTA2JnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE1NTkxMTcwMDgmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0='
+  API_KEY: 'API_KEY_HERE',
+  SESSION_ID: 'SESSION_ID_HERE',
+  TOKEN: 'TOKEN_HERE'
 };
